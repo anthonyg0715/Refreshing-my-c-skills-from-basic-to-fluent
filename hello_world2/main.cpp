2269a6e8af0138a7cef2d1cf0a87a003b2f955a0
anthonyg0715 <anthony_2457@hotmail.com>
2018-08-09 08:05:54 -0400

    Part 2 of new-line characters we showed endl in the first part now here we will explain the differences between endl and backslash n

---
+++ hello_world2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Welcome Back!" << endl;
+    cout << "Isn't using endl fun!" << endl;
+    cout << "endl means that this is where the line ends" << endl;
+    cout << "and it creates a new-line for the next line like this one for example \n";
+    cout << "Other than endl we can use backslash n which you can see in the code but not in the output even though it is in the quotes" << endl;
+    cout << "two differences of using either endl or backslash n is that endl flushes the stream and backslash n doesn't \n";
+    cout << "and finally that endl needs to be added after the quotes with another insertion operator before it and" << endl;
+    cout << "backslash n only needs to be in the quotes after you finish your line or you can have multiple "<< endl;
+    cout << "as \n you will see \n in the code \n";
+    return 0;
+}
