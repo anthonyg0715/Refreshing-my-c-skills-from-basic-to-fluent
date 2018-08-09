ec975c7daf85705b49b41be74a7d3508dda9bbb4
anthonyg0715 <anthony_2457@hotmail.com>
2018-08-09 07:31:38 -0400

    Hello World

---
+++ hello world/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//preprocessor directive which gives instructions to the compiler to process the information before
+//actual compilation starts
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;//basic output stream "<<" <-- these are called insertion operators
+    return 0;//when the compiler reaches this line it just means that everything else is fine
+}
